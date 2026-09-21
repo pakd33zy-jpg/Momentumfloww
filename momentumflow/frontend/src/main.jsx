cd062c1827c2de164fcad7eb6bd5d60f09d83e3c
pakd33zy-jpg <pakd33zy@gmail.com>
2026-09-21 15:15:25 -0700

    Clear stale PWA caches on app load

--- momentumflow/frontend/src/main.jsx
+++ momentumflow/frontend/src/main.jsx
@@ -5,6 +5,17 @@ import App from './App.jsx';
 import ErrorBoundary from './pages/ErrorBoundary.jsx';
 import './index.css';
 
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.getRegistrations()
+    .then((registrations) => Promise.all(registrations.map((registration) => registration.unregister())))
+    .catch(() => {});
+}
+if ('caches' in window) {
+  caches.keys()
+    .then((keys) => Promise.all(keys.map((key) => caches.delete(key))))
+    .catch(() => {});
+}
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ErrorBoundary>
